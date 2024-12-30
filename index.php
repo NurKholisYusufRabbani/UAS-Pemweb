1224cbdb5489f74178f19064a6d48072c691579e
Nur Kholis Yusuf Rabbani <nurkholis.23074@mhs.unesa.ac.id>
2024-12-30 19:31:13 +0700

    Del duplicate code

--- index.php
+++ index.php
@@ -99,11 +99,6 @@
                 </div>
             </div>
         </section>
-        
-        <!-- Bootstrap JS -->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-        <!-- Custom JS -->
-        <script src="assets/js/script.js"></script>
 
     </div>
     <footer class="text-center bg-dark py-2">
